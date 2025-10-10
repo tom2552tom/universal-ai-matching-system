9404cdca3499d49086d1f0ad2cbc689197540cb9
Rainy the cat <63931672+tom2552tom@users.noreply.github.com>
2025-10-10 06:59:23 -0700

    Update 1_ダッシュボード.py

--- 1_ダッシュボード.py
+++ 1_ダッシュボード.py
@@ -124,8 +124,8 @@ if not show_hidden_filter:
 
 if where_clauses: query += " WHERE " + " AND ".join(where_clauses)
 
-#query += " ORDER BY r.created_at DESC, r.score DESC LIMIT 100"
-query += " ORDER BY r.score desc LIMIT 100"
+query += " ORDER BY r.created_at DESC, r.score DESC LIMIT 100"
+
 results = conn.execute(query, tuple(params)).fetchall()
 conn.close()
 
