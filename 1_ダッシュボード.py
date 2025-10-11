fc58befbe98cff201f534b559c1e7d69ab0d7b73
Rainy the cat <63931672+tom2552tom@users.noreply.github.com>
2025-10-10 20:49:42 -0700

    Update 1_ダッシュボード.py

--- 1_ダッシュボード.py
+++ 1_ダッシュボード.py
@@ -81,13 +81,13 @@ status_options = [
     "採用", "見送り（自社都合）", "見送り（クライアント都合）", "見送り（技術者都合）", "クローズ"
 ]
 selected_statuses = st.sidebar.multiselect("進捗ステータス", options=status_options, placeholder="ステータスを選択して絞り込み")
-st.sidebar.divider()
+#st.sidebar.divider()
 grade_options = ['S','A', 'B', 'C', 'D', 'E']
 selected_grades = st.sidebar.multiselect("AI評価", options=grade_options, placeholder="評価を選択して絞り込み")
-st.sidebar.divider()
+#st.sidebar.divider()
 keyword_filter = st.sidebar.text_input("キーワード検索 (担当者名も可)")
-st.sidebar.divider()
-st.sidebar.header("ルールフィルター")
+#st.sidebar.divider()
+st.sidebar.header("その他フィルター")
 filter_nationality = st.sidebar.checkbox("「外国籍不可」の案件を除外する", value=False)
 show_hidden_filter = st.sidebar.checkbox("非表示も表示する", value=False)
 
